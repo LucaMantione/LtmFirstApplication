f7e9ddd91330ea5a6632daa9217d096065fc7c72
Luca Mantione <Luca Mantione>
2017-02-10 10:46:14 +0100

    secondoCommit

---
+++ app/src/main/java/com/zeroeur/primaapplicazione/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zeroeur.primaapplicazione;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Luca on 08/02/2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
